3c4a594506e8ad5912266c8afa8a822b1119464a
DrabornEagle <draborneagle@gmail.com>
2026-07-11 01:48:33 +0300

    security: harden v0.3 schedule seed helper

---
+++ supabase/migrations/20260711010600_v0_3_harden_seed_function.sql
@@ -0,0 +1,2 @@
+revoke execute on function public.seed_mechanic_working_hours() from public, anon, authenticated;
+grant execute on function public.seed_mechanic_working_hours() to postgres, service_role;
