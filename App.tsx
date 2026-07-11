a4a201490f9c0555c2946f27c04cdedd74b24b38
DrabornEagle <draborneagle@gmail.com>
2026-07-11 15:50:21 +0300

    feat: mount v0.8 notification provider and center

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { AppRoot } from './src/AppRoot';
 import { AuthProvider } from './src/context/AuthContext';
 import { ThemeProvider, useTheme } from './src/context/ThemeContext';
-import { AppRoot } from './src/AppRoot';
+import { NotificationCenterScreen } from './src/notifications/NotificationCenterScreen';
+import { NotificationProvider } from './src/notifications/NotificationContext';
 
 function StatusBarBridge() {
   const { resolvedMode } = useTheme();
@@ -15,8 +17,11 @@ export default function App() {
     <SafeAreaProvider>
       <ThemeProvider>
         <AuthProvider>
-          <StatusBarBridge />
-          <AppRoot />
+          <NotificationProvider>
+            <StatusBarBridge />
+            <AppRoot />
+            <NotificationCenterScreen />
+          </NotificationProvider>
         </AuthProvider>
       </ThemeProvider>
     </SafeAreaProvider>
